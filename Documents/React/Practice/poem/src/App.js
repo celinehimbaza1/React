1d49e5e3d2b5cab4917d29a1bea0b7e24f54297d
celine <celine.himbaza@gmail.com>
2025-04-16 16:51:42 +0200

    Poem

---
+++ Documents/React/Practice/poem/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './components/Counter';
+import { Poem } from './components/Poem';
+
+function App() {
+  return (
+    <div className="App ">
+    {/* <Counter/> */}
+    <Poem/>
+    </div>
+  );
+}
+
+export default App;
